40c7c5b59c2da7590b6a008bbccc63061b7b0592
gabrielruizmb <gabrielruizmb@gmail.com>
2024-10-21 19:54:26 -0300

    Adicionado teste do sensor ultrasônico

--- robot-code/robot-code.ino
+++ robot-code/robot-code.ino
@@ -4,28 +4,54 @@
 int lineSensor1Value = 0;
 int lineSensor2Value = 0;
 
+int trigger = 6;
+int eco = 7;
+float robotDistance;
 
-void setup() {
+float distance(int trigger, int eco);
+
+void setup() 
+{
   // put your setup code here, to run once:
 
   Serial.begin(9600);
  
   pinMode(lineSensor1, INPUT);
   pinMode(lineSensor2, INPUT);
+  pinMode(trigger, OUTPUT);
 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
 
   lineSensor1Value = analogRead(lineSensor1);
   lineSensor2Value = analogRead(lineSensor2);
+
+  // Se estiver dentro da pista o valor é maior que 800, na linha menor que 600
+//  Serial.print("Valor do sensor 1: ");
+//  Serial.println(lineSensor1Value);
+//
+//  Serial.print("Valor do sensor 2: ");
+//  Serial.println(lineSensor2Value);
+
+  robotDistance = distance(trigger, eco);
+
+  //robotDistance = sensor.getCM();
   
-  Serial.println("Valor do sensor 1: ");
-  Serial.println(lineSensor1Value);
+  Serial.println(robotDistance);
+  delay(2000);
+}
 
-  Serial.println("Valor do sensor 2: ");
-  Serial.println(lineSensor2Value);
-  delay(1000);
-
+float distance(int trigger, int eco) 
+{
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(trigger, LOW);
+  pinMode(eco, INPUT);
+  
+  return (pulseIn(eco, HIGH)*0.01723);
 }
